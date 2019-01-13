b2dc5ac363ed1b122916ec779c4ea5e628948a8b
Christian Kniep <christian@qnib.org>
2019-01-13 11:39:38 +0100

    update start-jupyter-notebook

--- start-jupyter-notebook.sh
+++ start-jupyter-notebook.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 : ${JUPYTER_NOTEBOOK_DIR:=/notebooks}
+: ${JUPYTERPORT_BASE_DIR:=/data}
+: ${JUPYTERPORT_SAVE_PATH:=/data/save}
+: ${JUPYTER_BASE_URL:=/}
 
-jupyter notebook --ip=0.0.0.0 --allow-root --notebook-dir=${JUPYTER_NOTEBOOK_DIR}
+OPTS=""
+if [[ "X${JUPYTER_API_TOKEN}" != "X" ]];then
+  OPTS="${OPTS} --NotebookApp.token=${JUPYTER_API_TOKEN}"
+fi
+
+cd ${JUPYTERPORT_BASE_DIR}
+echo "jupyter notebook --ip=0.0.0.0 --allow-root ${OPTS} \\"
+echo "                 --notebook-dir=${JUPYTER_NOTEBOOK_DIR} \\"
+echo "                 --NotebookApp.base_url=${JUPYTER_BASE_URL}"
+jupyter notebook --ip=0.0.0.0 --allow-root ${OPTS} \
+                 --notebook-dir=${JUPYTER_NOTEBOOK_DIR} \
+                 --NotebookApp.base_url=${JUPYTER_BASE_URL}
